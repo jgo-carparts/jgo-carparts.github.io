00c71586f37ccd1eb4fce204075b76d583f38fc6
jgo-carparts <jgo.carparts@gmail.com>
2026-02-23 15:43:47 +0800

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,69 @@
+header ='<!doctype html>\
+<html lang="zh-Hant-TW">\
+\
+        <!--========== FOOTER ==========-->\
+        <footer class="g-bg-color--dark">\
+            <!-- Links -->\
+            <div class="g-hor-divider__dashed--white-opacity-lightest">\
+                <div class="container g-padding-y-80--xs">\
+                    <div class="row">\
+                        <div class="col-sm-2 g-margin-b-20--xs g-margin-b-0--md">\
+                            <ul class="list-unstyled g-ul-li-tb-5--xs g-margin-b-0--xs">\
+                                <li><a style="color:#1aa7d3">About & Legal</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="JGO.html">關於捷國 | About Us</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="https://lihi.cc/eQHJA" target="_blank">零件諮詢 | Parts Inquiry</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="Partnership.html" target="_blank">商業合作 | Partnership</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="FAQ.html">常見問題 | FAQ</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="Privacy.html">隱私政策 | Privacy</a></li>\
+                            </ul>\
+                            </ul>\
+                        </div>\
+                        <div class="col-sm-2 g-margin-b-40--xs g-margin-b-0--md">\
+                            <ul class="list-unstyled g-ul-li-tb-5--xs g-margin-b-0--xs">\
+                                \
+                                <li><a style="color:#1aa7d3">Applications</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="Mazda.html">Mazda 馬自達</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="Ford.html">Ford 福特</a></li>\
+                            </ul>\
+                        </div>\
+                        <div class="col-sm-2 g-margin-b-40--xs g-margin-b-0--md">\
+                            <ul class="list-unstyled g-ul-li-tb-5--xs g-margin-b-0--xs">\
+                                \
+                                <li><a style="color:#1aa7d3">Brand Partners</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="SACHS.html">SACHS 薩克斯</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="BOSCH.html">BOSCH 博世</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="Brembo">Brembo 布雷博</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="MEYLE.html">MEYLE 梅爾</a></li>\
+                            </ul>\
+                        </div>\
+                        <div class="col-md-4 col-md-offset-2 col-sm-5 col-sm-offset-1 s-footer__logo g-padding-y-50--xs g-padding-y-0--md">\
+                            <h3 class="g-font-size-18--xs g-color--white">JGO 捷國國際</h3>\
+                            <p class="g-color--white-opacity">身為值得信賴的汽車零件供應商，我們專注於優質零件、正規貨源、貼心售後服務，並以專業技術支援確保您的行車無憂。</p>\
+                            <li><a class="g-font-size-15--xs g-color--white-opacity" href="https://lihi.cc/RdMCY" target="_blank">LINE 線上客服</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" >營業時間 | 週一至週五 9:00 - 18:00</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" >地址 | 新北市中和區中正路 733 號 5F</a></li>\
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" >統編 | 54943392</a></li>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+            <!-- End Links -->\
+\
+            <!-- Copyright -->\
+            <div class="container g-padding-y-50--xs">\
+                <div class="row">\
+                    <div class="col-xs-6">\
+                        <a href="index.html">\
+                            <img class="g-width-100--xs g-height-auto--xs" src="img/logo-white.png" alt="Megakit Logo">\
+                        </a>\
+                    </div>\
+                    <div class="col-xs-6 g-text-right--xs">\
+                        <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white-opacity-light">Copyright © 2026 <br><a>JGO 捷國國際</a></p>\
+                    </div>\
+                </div>\
+            </div>\
+            <!-- End Copyright -->\
+        </footer>\
+        <!--========== END FOOTER ==========-->\
+</nav>';
+document.write(header);
---
+++ js/menu_Corporate.js
@@ -0,0 +1,105 @@
+header ='<!doctype html>\
+<html lang="zh-Hant-TW">\
+\
+        <!--========== HEADER ==========-->\
+        <header class="navbar-fixed-top s-header js__header-sticky js__header-overlay">\
+            <!-- Navbar -->\
+            <div class="s-header__navbar">\
+                <div class="s-header__container">\
+                    <div class="s-header__navbar-row">\
+                        <div class="s-header__navbar-row-col">\
+                            <!-- Logo -->\
+                            <div class="s-header__logo">\
+                                <a href="index.html" class="s-header__logo-link">\
+                                    <img class="s-header__logo-img s-header__logo-img-default" src="img/logo-white.png" alt="Megakit Logo">\
+                                    <img class="s-header__logo-img s-header__logo-img-shrink" src="img/logo-dark.png" alt="Megakit Logo">\
+                                </a>\
+                            </div>\
+                            <!-- End Logo -->\
+                        </div>\
+                        <div class="s-header__navbar-row-col">\
+                            <!-- Trigger -->\
+                            <a href="javascript:void(0);" class="s-header__trigger js__trigger">\
+                                <span class="s-header__trigger-icon"></span>\
+                                <svg x="0rem" y="0rem" width="3.125rem" height="3.125rem" viewbox="0 0 54 54">\
+                                    <circle fill="transparent" stroke="#fff" stroke-width="1" cx="27" cy="27" r="25" stroke-dasharray="157 157" stroke-dashoffset="157"></circle>\
+                                </svg>\
+                            </a>\
+                            <!-- End Trigger -->\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+            <!-- End Navbar -->\
+\
+            <!-- Overlay -->\
+            <div class="s-header-bg-overlay js__bg-overlay">\
+                <!-- Nav -->\
+                <nav class="s-header__nav js__scrollbar">\
+                    <div class="container-fluid">\
+                        <!-- Menu List -->\
+                        <ul class="list-unstyled s-header__nav-menu">\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider -is-active" href="index.html">首頁 | Home</a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="JGO.html">關於捷國 | About</a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="https://lihi.cc/eQHJA" target="_blank">零件諮詢 | Inquiry</a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="Partnership.html" target="_blank">商業合作 | Partnership</a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="FAQ.html">常見問題 | FAQ</a></li>\
+                        </ul>\
+                        <!-- End Menu List -->\
+\
+                        <!-- Menu List -->\
+                        <ul class="list-unstyled s-header__nav-menu">\
+                            <li class="s-header__nav-menu-item"><a style="color:#1aa7d3"; class="s-header__nav-menu-link s-header__nav"><b>Applications</b></a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="Mazda.html">Mazda 馬自達</a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="Ford.html">Ford 福特</a></li>\
+                        </ul>\
+                        <!-- End Menu List -->\
+\
+                        <!-- Menu List -->\
+                        <ul class="list-unstyled s-header__nav-menu">\
+                            <li class="s-header__nav-menu-item"><a style="color:#1aa7d3"; class="s-header__nav-menu-link s-header__nav"><b>Brand Partners</b></a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="SACHS.html">SACHS 薩克斯</a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="BOSCH.html">BOSCH 博世</a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="Brembo.html">Brembo 布雷博</a></li>\
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="MEYLE.html">MEYLE 梅爾</a></li>\
+                        <br></ul>\
+                        <!-- End Menu List -->\
+                    </div>\
+                </nav>\
+                <!-- End Nav -->\
+\
+                <!-- Action -->\
+                <!-- <ul class="list-inline s-header__action s-header__action--lb">\
+                    <li class="s-header__action-item"><a class="s-header__action-link -is-active" href="#">En</a></li>\
+                    <li class="s-header__action-item"><a class="s-header__action-link" href="#">Fr</a></li>\
+                </ul> -->\
+                <!-- End Action -->\
+\
+                <!-- Action -->\
+                <!-- <ul class="list-inline s-header__action s-header__action--rb">\
+                    <li class="s-header__action-item">\
+                        <a class="s-header__action-link" href="#">\
+                            <i class="g-padding-r-5--xs ti-facebook"></i>\
+                            <span class="g-display-none--xs g-display-inline-block--sm">Facebook</span>\
+                        </a>\
+                    </li>\
+                    <li class="s-header__action-item">\
+                        <a class="s-header__action-link" href="#">\
+                            <i class="g-padding-r-5--xs ti-twitter"></i>\
+                            <span class="g-display-none--xs g-display-inline-block--sm">Twitter</span>\
+                        </a>\
+                    </li>\
+                    <li class="s-header__action-item">\
+                        <a class="s-header__action-link" href="#">\
+                            <i class="g-padding-r-5--xs ti-instagram"></i>\
+                            <span class="g-display-none--xs g-display-inline-block--sm">Instagram</span>\
+                        </a>\
+                    </li>\
+                </ul> -->\
+                <!-- End Action -->\
+            </div>\
+            <!-- End Overlay -->\
+        </header>\
+        <!--========== END HEADER ==========-->\
+</nav>';
+document.write(header);
